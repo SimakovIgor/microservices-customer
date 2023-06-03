f5c623ec7e2f0d7706d4a6b5c6cb2a1b96518bf5
Igor Simakov <i.simakov@banki.ru>
2023-06-03 14:26:12 +0800

    + sync NotificationClient service

--- src/main/java/ru/simakov/service/CustomerService.java
+++ src/main/java/ru/simakov/service/CustomerService.java
@@ -2,8 +2,9 @@ package ru.simakov.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.simakov.clients.fraud.FraudCheckResponse;
 import ru.simakov.clients.fraud.FraudClient;
+import ru.simakov.clients.notification.NotificationClient;
+import ru.simakov.clients.notification.NotificationRequest;
 import ru.simakov.model.dto.CustomerRegistrationRq;
 import ru.simakov.model.entity.Customer;
 import ru.simakov.repository.CustomerRepository;
@@ -13,13 +14,27 @@ import ru.simakov.repository.CustomerRepository;
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
 
-    public void registerCustomer(CustomerRegistrationRq customerRegistrationRq) {
-        Customer customer = Customer.builder()
+    private static Customer mapCustomer(CustomerRegistrationRq customerRegistrationRq) {
+        return Customer.builder()
                 .firstName(customerRegistrationRq.getFirstName())
                 .lastName(customerRegistrationRq.getLastName())
                 .email(customerRegistrationRq.getEmail())
                 .build();
+    }
+
+    private static NotificationRequest getNotificationRequest(Customer customer) {
+        return NotificationRequest.builder()
+                .message("Welcome, %s %s!!".formatted(customer.getEmail(), customer.getFirstName()))
+                .toCustomerId(customer.getId())
+                .toCustomerEmail(customer.getEmail())
+                .sender("Customer microservice")
+                .build();
+    }
+
+    public void registerCustomer(CustomerRegistrationRq customerRegistrationRq) {
+        var customer = mapCustomer(customerRegistrationRq);
         customerRepository.save(customer);
 
         var fraudCheckResponse = fraudClient.getIsFraudster(customer.getId());
@@ -27,5 +42,6 @@ public class CustomerService {
             throw new IllegalStateException("Customer %s is fraudster".formatted(customer.getId()));
         }
 
+        notificationClient.sendNotification(getNotificationRequest(customer));
     }
 }
