f15814059cab67954ffcc466557cadcfe9da807b
Igor Simakov <i.simakov@banki.ru>
2023-06-04 13:08:30 +0800

    Enable Distributed Tracing using Micrometer, OpenTelemetry, And Jaeger

--- src/main/java/ru/simakov/controller/CustomerController.java
+++ src/main/java/ru/simakov/controller/CustomerController.java
@@ -1,5 +1,8 @@
 package ru.simakov.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,14 @@ import ru.simakov.service.CustomerService;
 
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController(CustomerService customerService) {
+@RequiredArgsConstructor
+@Log4j2
+public class CustomerController {
+    private final CustomerService customerService;
+
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRq customerRegistrationRq) {
+        log.info("New registerCustomer {}", customerRegistrationRq);
         customerService.registerCustomer(customerRegistrationRq);
     }
 }
