479a000664cab8043c1889d05bbf377ff7528b17
Igor Simakov <i.simakov@banki.ru>
2023-06-07 14:49:36 +0800

    add rabbit mq

---
+++ src/main/java/ru/simakov/config/RabbitMQProperties.java
@@ -0,0 +1,14 @@
+package ru.simakov.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class RabbitMQProperties {
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String internalNotificationRoutingKey;
+}
--- src/main/java/ru/simakov/service/CustomerService.java
+++ src/main/java/ru/simakov/service/CustomerService.java
@@ -3,18 +3,20 @@ package ru.simakov.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.simakov.clients.fraud.FraudClient;
-import ru.simakov.clients.notification.NotificationClient;
 import ru.simakov.clients.notification.NotificationRequest;
+import ru.simakov.config.RabbitMQProperties;
 import ru.simakov.model.dto.CustomerRegistrationRq;
 import ru.simakov.model.entity.Customer;
 import ru.simakov.repository.CustomerRepository;
+import ru.simakov.starter.amqp.config.RabbitMQMessageProducer;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
+    private final RabbitMQProperties rabbitMqProperties;
 
     private static Customer mapCustomer(CustomerRegistrationRq customerRegistrationRq) {
         return Customer.builder()
@@ -42,7 +44,8 @@ public class CustomerService {
             throw new IllegalStateException("Customer %s is fraudster".formatted(customer.getId()));
         }
 
-        notificationClient.sendNotification(getNotificationRequest(customer));
+        rabbitMQMessageProducer.publish(getNotificationRequest(customer),
+                rabbitMqProperties.getInternalExchange(), rabbitMqProperties.getInternalNotificationRoutingKey());
         return customer;
     }
 }
